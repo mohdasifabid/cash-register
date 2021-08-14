e011b4893757b1fd50d44812285917718c6a4d41
asif <masifabid@outlook.com>
2021-08-14 15:38:50 +0530

    button

---
+++ cashregister.js
@@ -0,0 +1,9 @@
+
+<textarea id="cashInput"></textarea>
+var billInput = document.querySelector("#billInput")
+var btnNext = document.querySelector("btnNext")
+
+btnNext.addEventListener("click", function() {
+    var userInput = btnNext.nodeValue;
+
+})
